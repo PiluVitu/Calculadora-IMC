108e3241fbde942f51df898675b05183d2980c58
Paulo Victor <paulo.tspi@gmail.com>
2022-09-02 10:32:20 -0300

    js core complete

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const submitButton = document.querySelector('.mainCard button')
-const weight = document.querySelector('')
+const closeButton = document.querySelector('.popUpCard button')
+const weight = document.querySelector('#peso')
+const height = document.querySelector('#altura')
+const textPopUpCard = document.querySelector('.popUpCard h2')
+const popUpCard = document.querySelector('.popUpCardWrapper')
+const textError = document.querySelector('.textError')
 
-submitButton.addEventListener('click',test)
+submitButton.addEventListener('click', submit)
+closeButton.addEventListener('click', showAndClosePopUp)
 
-function test(event){
+function submit(event) {
   event.preventDefault()
+  let pesoTeste = Number(weight.value)
+  let alturaTeste = Number(height.value)
+  const calcImc = (pesoTeste / alturaTeste ** 2).toFixed(1)
+  if (calcImc >= 10) {
+    showAndClosePopUp()
+    textPopUpCard.innerHTML = `Seu IMC é de ${calcImc}`
+  } else {
+    showAndCloseTextError()
+    setTimeout(showAndCloseTextError, 2000)
+  }
+}
+function showAndCloseTextError() {
+  textError.classList.toggle('open')
+}
+function showAndClosePopUp() {
+  popUpCard.classList.toggle('open')
 
-  console.log('Passou porra')
+  weight.value = ''
+  height.value = ''
 }
